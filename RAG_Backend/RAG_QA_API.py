823b7bdbc3a1d82057aa529b742cf9f3642ebdee
JingtaoXie <jxie275@aucklanduni.ac.nz>
2026-01-02 23:55:11 +0800

    #20260102/ProjectStartUp/questionAndResponseFunctionBasicallyReadyToGo/FineTuneModel

--- RAG_Backend/RAG_QA_API.py
+++ RAG_Backend/RAG_QA_API.py
@@ -1,18 +1,41 @@
-from langchain_ollama import ChatOllama ##langchain ollama package
-
+import time
+from langchain_ollama import ChatOllama   ##langchain ollama package
+import VOICE_INPUT_OUTPUT as vio
+from functools import lru_cache
 
 
 llm = ChatOllama(
     model="phi3",
     temperature=0,
 )
+@lru_cache(maxsize=30)
+def questionAndResponse():
+    print("I am listening your question ")
+    clientCommand = vio.startListening()
+    vio.sd.wait()
+    clientCommandinText = vio.identfyingCommand(clientCommand)
 
-messages = [
-    ("system", "You are a Chinese AI. Answer all questions in Chinese."),
-    ("human", "Hello, how are you?"),
-]
+    if clientCommandinText == "":
+        vio.readingAnswer("Sorry I didn't follow your questions")
+    else:
+        time.sleep(0.5)
+        messages = [
+            ("system", "Answer all questions in Chinese."),
+            ("human", clientCommandinText),
+        ]
+        ai_msg = llm.invoke(messages)
 
-ai_msg = llm.invoke(messages)
+        # invoke can return lots of elements and content is the answer itself
+        print(ai_msg.content)
+        vio.readingAnswer(ai_msg.content)
 
-#模型invoke返回的信息包含很多参数，其中content里的才为回答本身
-print(ai_msg.content)
+
+if __name__ == "__main__":
+    print("===== Program Starting =====", flush=True)
+    questionAndResponse()
+
+
+
+
+
+
--- RAG_Backend/VOICE_INPUT_OUTPUT.py
+++ RAG_Backend/VOICE_INPUT_OUTPUT.py
@@ -1,16 +1,16 @@
 import sounddevice as sd  ##voice recognizer package
 import vosk ##voice recognizer model
+import pyttsx3 ##from text to voice
 import queue
 import json
 
 model_path="vosk-model-small-cn-0.22"
 
-q = queue.Queue()
-duration = 3
-sample_rate = 16000 ##normally 16000 Hz, should align with the sample rate for model training
+# q = queue.Queue()
+duration = 5
+sample_rate = 16000   ##normally 16000 Hz, should align with the sample rate for model training
 sd.default.samplerate = sample_rate
 sd.default.channels = 1
-
 
 def startListening():
     clientCommand = sd.rec(int(duration * sample_rate), dtype='int16')
@@ -25,15 +25,23 @@ def identfyingCommand(clientCommand):
     result = json.loads(rec.Result())
     return result.get("text", "")
 
+def readingAnswer(clientCommandinText):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 200)  # speaking rate
+    engine.setProperty('volume', 1.0)  # volume
+    engine.say(clientCommandinText)
+    engine.runAndWait()  # playing text
 
-def startSpeaking():
-    sd.play()
-    sd.wait()
+# def startSpeaking(clientCommandinText):
+#     sd.play(clientCommandinText)
 
 
-clientCommand = startListening()
-clientCommandinText = identfyingCommand(clientCommand)
-print(clientCommandinText)
+## Testing Command
+
+# clientCommand = startListening()
+# clientCommandinText = identfyingCommand(clientCommand)
+# print(clientCommandinText)
+# readingAnswer(clientCommandinText)
 
 
 
